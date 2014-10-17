b969de9191236eb9c0dae43e7e620ac63aad73ed
Dipak Patil <dipakupatil@gmail.com>
2014-10-17 09:37:01 +0530

    updated data

--- app/admin/course.rb
+++ app/admin/course.rb
@@ -1,6 +1,21 @@
 ActiveAdmin.register Course do
-  permit_params :title, :description, :duration, :fees, :feature_image, :status, :batch
+  permit_params :title, :description, :duration, :fees, :pic, :status, :batch
    
+  
+    form :partial => "form"
+=begin
+
+   form do |f|
+      f.inputs "Details" do
+        f.input :title
+        f.input :description
+        f.input :duration
+    
+      f.actions
+    end
+  end
+=end
+end
 =begin
   index do |f|
     f.input :title
@@ -25,7 +40,4 @@ ActiveAdmin.register Course do
    # permitted = [:permitted, :attributes]
    #permitted << :other if resource.something?
    # permitted
-  #end
 
-
-end
