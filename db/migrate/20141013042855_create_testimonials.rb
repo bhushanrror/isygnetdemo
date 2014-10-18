ebdec0eeefe13819a9b4156d6f17c04dd15d3cdf
bhushanrror <b.bhushan@starkdigital.net>
2014-10-18 18:58:56 +0530

    Update 20141013042855_create_testimonials.rb

--- db/migrate/20141013042855_create_testimonials.rb
+++ db/migrate/20141013042855_create_testimonials.rb
@@ -2,8 +2,7 @@ class CreateTestimonials < ActiveRecord::Migration
   def change
     create_table :testimonials do |t|
     	t.string :title
-    	t.text :body
-    	t.text :type
+    	t.text :videourl
     	t.boolean :status
         t.string :pic_file_name
         t.string :pic_content_type
