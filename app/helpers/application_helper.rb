a496e9f99e89cd0d4544096ab86bda61824c5a36
b.bhushan@starkdigital.net <b.bhushan@starkdigital.net>
2014-10-21 10:49:44 +0530

    menu active enabled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def is_active?(link_path)
+		puts request.fullpath
+		puts link_path
+  		if (request.fullpath == link_path) then 
+  			"active"
+  		elsif ((request.fullpath.include? 'course') &&  (link_path.include? 'course' ))
+  			"active"
+  		end
+	end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :title
+	validates_uniqueness_of :title
 	has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>",:land => "250x100>" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
 end
