ca09be83fa0c26a3b92cb7b5c2cfb9fdb19ce77e
b.bhushan@starkdigital.net <b.bhushan@starkdigital.net>
2014-10-21 16:36:25 +0530

    admin panel form multipart added

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,6 @@ class Course < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :title
 	validates_uniqueness_of :title
-	has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>",:land => "250x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
 end
