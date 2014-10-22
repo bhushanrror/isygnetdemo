301a14dcc2769afc558c70243f4c3eacb79562d3
b.bhushan@starkdigital.net <b.bhushan@starkdigital.net>
2014-10-22 14:45:42 +0530

    friendly slug module added

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,6 @@
 class Course < ActiveRecord::Base
 	extend FriendlyId
-	friendly_id :title
-	validates_uniqueness_of :title
+	friendly_id :title, use: [:slugged, :history]
 	has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  	validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
+  	validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/  	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017135852) do
+ActiveRecord::Schema.define(version: 20141022065150) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -58,7 +58,23 @@ ActiveRecord::Schema.define(version: 20141017135852) do
     t.datetime "pic_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
+
+  add_index "courses", ["slug"], name: "index_courses_on_slug", unique: true
+
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "placements", force: true do |t|
     t.string   "title"
@@ -87,6 +103,14 @@ ActiveRecord::Schema.define(version: 20141017135852) do
   create_table "settings", force: true do |t|
     t.string   "settingkey"
     t.string   "settingvalue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "templates", force: true do |t|
+    t.string   "title"
+    t.string   "nameslug"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
