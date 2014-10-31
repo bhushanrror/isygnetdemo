dc71ca1d230491d61362d3e9cd5d0eca48df26e8
b.bhushan@starkdigital.net <b.bhushan@starkdigital.net>
2014-10-31 19:03:29 +0530

    email not working

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: 'bbhushanb89@gmail.com'
+  default from: 'b.bhushan@starkdigital.net'
  
   def welcome_email(user)
     @user = user
-    @url  = 'http://google.com'
+    @url  = 'http://www.starkdigital.net'
     
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
